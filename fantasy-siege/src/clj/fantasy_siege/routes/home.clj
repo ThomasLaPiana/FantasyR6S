3b2cfdf774b803cb4a494c07408291b0612d6128
Thomas La Piana <tal103020@icloud.com>
2017-09-09 22:43:59 -0500

    wrote a plain-text csv parsing function to integrate with the upload saving function

--- fantasy-siege/src/clj/fantasy_siege/routes/home.clj
+++ fantasy-siege/src/clj/fantasy_siege/routes/home.clj
@@ -2,17 +2,29 @@
   (:require [fantasy-siege.layout :as layout]
             [compojure.core :refer [defroutes GET POST]]
             [ring.util.http-response :as response]
+            [clojure.string :refer [split-lines trim]]
+            [csv-map.core :refer [parse-csv]]
             [clojure.java.io :as io]))
 
 (defn home-page []
   (layout/render "home.html"))
 
+(defn parse-upload [csv-data]
+  "Parse the uploaded data into a seq of hashmaps"
+  (-> csv-data
+      (parse-csv :key :keyword)))
+
 (defn save-upload! [{:keys [params]}]
-  (let [row (-> params :data)
-        table (-> params :table)]
-  (println row table)
-  (response/ok {:status :ok})))
+  "Upload the data submitted data by the admins through the upload page"
+  (let [table (-> params :table)
+        data-seq (parse-upload (-> params :data))]
+    (do (println table data-seq)
+        (response/ok {:status :ok}))))
+
+(save-upload! {:params {:data "name\ntest" :table "teams"}})
+
 
 (defroutes home-routes
+  "Define the home-routes"
   (GET "/" [] (home-page))
   (POST "/upload" req (save-upload! req)))
