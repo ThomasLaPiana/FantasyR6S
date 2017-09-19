239943d4a5bc7d081968e18714da461611aad643
Thomas La Piana <tal103020@icloud.com>
2017-09-18 21:05:28 -0500

    add dummy data and a script to upload csv files to the database

---
+++ fantasy-siege/seed-data/data-upload.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+file_list = ['matches','players','teams']
+file_dict = {}
+engine = create_engine('postgresql://fsig_dev:fsig_dev_pw@localhost/fantasy_siege_dev')
+
+for file in file_list:
+    df = pd.read_csv('{}.csv'.format(file))
+    df.to_sql(file,engine,if_exists='replace')
+    print('uploaded {} file'.format(file))
