c212fe0f6f1ca6c479d800fdb28210d8ced8a3e1
Thomas La Piana <tal103020@icloud.com>
2017-09-18 21:05:02 -0500

    update the matches table to a more final form

--- fantasy-siege/resources/migrations/20170909220552-matches.up.sql
+++ fantasy-siege/resources/migrations/20170909220552-matches.up.sql
@@ -1,5 +1,11 @@
 CREATE TABLE matches
 (id VARCHAR(30) PRIMARY KEY,
+  slot INTEGER,
   year INTEGER,
   season INTEGER,
-  week INTEGER);
+  region VARCHAR(10),
+  week INTEGER,
+  player VARCHAR(50) REFERENCES players(alias),
+  kills INTEGER,
+  deaths INTEGER,
+  assists INTEGER;)
